2fb5ef0d2eda237a3d683009e6daf2108457422c
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-10-23 22:57:51 +0500

    ✨ NEW FEATURE: deployment

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ app.use("/api/users", userRoutes);
 
 console.log("Hello");
 
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
