9d39b15649ce48ac1428b41ead5f7772ef647945
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-10-24 21:48:30 +0500

    changings

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
-import useSignup from "../../hooks/useSignUp";
+import useSignup from "../../hooks/useSignup.js";
 
 const SignUp = () => {
   const [input, setInputs] = useState({
